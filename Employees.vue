bf31398ff0d269e2e24c69ed823e1920d902d342
Juana Molina <49993369+juanamolinalucia2001@users.noreply.github.com>
2023-05-03 11:32:38 -0300

    Add files via upload

---
+++ Employees.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>Nuevo Auto</h1>
+    <FormularioGenerico :objeto="auto" storeAction="guardarAuto" />
+  </div>
+</template>
+
+<script>
+import FormularioGenerico from '@/components/commons/FormGeneric.vue'
+
+export default {
+  name: 'NuevoAuto',
+  components: {
+    FormularioGenerico
+  },
+  data() {
+    return {
+      auto: [ 
+        {name: 'marca', type:'text',data:null},
+        {name: 'modelo', type:'text',data:null},
+        {name: 'año', type:'number',data:null},
+      ]
+    }
+  },
+  methods: {
+    guardarAuto(auto) {
+      // Llama a la acción de Vuex para guardar el objeto "auto" en el estado
+      console.log(auto)
+    }
+  }
+}
+</script>
